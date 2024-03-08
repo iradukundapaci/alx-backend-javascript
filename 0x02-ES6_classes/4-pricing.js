c93f430e3efa93119e67f4509b2974e0c4f33357
iradukundapaci <paifique@gmail.com>
2024-03-08 18:07:25 +0200

    adding task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,53 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount should be number');
+    }
+
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency should be instance of class Currency');
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount should be number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Number) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency should be instance of class Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (!(typeof amount === 'number') || !(conversionRate instanceof Number)) {
+      throw new TypeError('Amount and ConversionRate should be numbers');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
