066b628fb7cfebcd91665228c0111b71df0d319a
iradukundapaci <paifique@gmail.com>
2024-03-12 09:53:10 +0200

    adding task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,22 @@
-export default function cleanSet() {}
+export default function cleanSet(set, startString) {
+  if (
+    set instanceof Set
+    && typeof startString === 'string'
+    && startString.length > 0
+  ) {
+    const modifiedValues = Array.from(set)
+      .filter((item) => item.startsWith(startString))
+      .map((item) => item.slice(startString.length))
+      .join('-');
+    return modifiedValues;
+  }
+  return '';
+}
+
+console.log(
+  cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'),
+);
+
+console.log(
+  cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''),
+);
