b2bfe0f3099790daba9db946abae72e15cc1c485
iradukundapaci <paifique@gmail.com>
2024-03-12 08:28:30 +0200

    adding paramenter validation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process');
+  }
   return map.forEach((value, index) => {
     if (value === 1) {
       map.set(index, 100);
