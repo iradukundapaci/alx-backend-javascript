6b3ad517e516a74e44c067dcc9b88ffabf34abd6
iradukundapaci <paifique@gmail.com>
2024-04-30 09:42:18 +0200

    adding server export

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,16 @@
-const http = require('node:http');
+const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
