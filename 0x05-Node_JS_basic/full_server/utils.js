2ddd38891585d54c16a59dfe1c0cc4d5d4d7668d
iradukundapaci <paifique@gmail.com>
2024-05-02 16:44:35 +0200

    adding task 8

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+
+export default function readDatabase(filepath) {
+  if (fs.existsSync(filepath)) {
+    return new Promise((resolve) => {
+      fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+          throw Error('Cannot load the database');
+        }
+        const result = [];
+        data.split('\n').forEach((data) => {
+          result.push(data.split(','));
+        });
+        result.shift();
+        const newis = [];
+        result.forEach((data) => newis.push([data[0], data[3]]));
+        const fields = new Set();
+        newis.forEach((item) => fields.add(item[1]));
+        const final = {};
+        fields.forEach((data) => {
+          final[data] = 0;
+        });
+        newis.forEach((data) => {
+          final[data[1]] += 1;
+        });
+        console.log(
+          `Number of students: ${
+            result.filter((check) => check.length > 3).length
+          }`,
+        );
+        Object.keys(final).forEach((data) => {
+          console.log(
+            `Number of students in ${data}: ${final[data]}. List: ${newis
+              .filter((n) => n[1] === data)
+              .map((n) => n[0])
+              .join(', ')}`,
+          );
+        });
+        resolve(result, final, newis);
+      });
+    });
+  }
+  throw Error('Cannot load the database');
+}
